8a0dccecd2ce0e45da31b26865e69728863e7db4
Raph harcyah Bonnet <harcyah@gmail.com>
2019-01-09 08:45:38 +0100

    WIP

--- HeroProfiler.lua
+++ HeroProfiler.lua
@@ -1,18 +1,3 @@
-local function ExportProfession(index)
-	local profession = {}
-	if (index == nil) then
-		profession.index = nil
-		profession.name = ""
-		profession.levels = {}
-	else
-		local name = GetProfessionInfo(index)
-		profession.index = index
-		profession.name = name
-		profession.levels = {}
-	end
-	return profession
-end
-
 local function ExportBackpack()
 	local bag = {}
 	bag.link = nil
@@ -29,7 +14,11 @@ local function ExportContainer(index)
 	return bag
 end
 
-local function ExportProfiles() 
+local function ClearProfiles()
+	HeroProfiles = {}
+end
+
+local function ExportProfiles()
 	HeroProfiles.name = UnitName("player")
 	HeroProfiles.realm = GetRealmName()
 	HeroProfiles.level = UnitLevel("player")
@@ -41,38 +30,38 @@ local function ExportProfiles()
 	HeroProfiles.xpMax = UnitXPMax("player")
 	HeroProfiles.health = UnitHealthMax("player")
 	HeroProfiles.side = UnitFactionGroup("player")
-	
+
 	local restId, restName = GetRestState()
 	HeroProfiles.restId = restId
 	HeroProfiles.restName = restName
-	
+
 	local avgItemLevel, avgItemLevelEquipped, avgItemLevelPvp = GetAverageItemLevel()
 	HeroProfiles.avgItemLevel = avgItemLevel
 	HeroProfiles.avgItemLevelEquipped = avgItemLevelEquipped
 	HeroProfiles.avgItemLevelPvp = avgItemLevelPvp
-	
+
 	HeroProfiles.hasMasterRiding = tostring(IsSpellKnown(90265))
-	
+
 	local className, classFile, classID = UnitClass("player");
 	HeroProfiles.clazz = classFile
-	
+
 	local raceName, raceFile, raceID = UnitRace("player")
 	HeroProfiles.race = raceFile
-	
+
 	local englishFaction, localizedFaction = UnitFactionGroup("player")
 	HeroProfiles.faction = englishFaction
-	
+
 	local specIndex = GetSpecialization()
 	local specId, specName, _, _, _, specRole = GetSpecializationInfo(specIndex)
 	HeroProfiles.specId = specId
 	HeroProfiles.specName = specName
 	HeroProfiles.specRole = GetSpecializationRoleByID(specId)
-	
+
 	local totalAchievements, completedAchievements = GetNumCompletedAchievements()
 	HeroProfiles.totalAchievements = totalAchievements
 	HeroProfiles.completedAchievements = completedAchievements
 	HeroProfiles.totalAchievementPoints = GetTotalAchievementPoints()
-	
+
 	HeroProfiles.factions = {}
 	for i=1,GetNumFactions() do
 		local name, description, standingId, bottomValue, topValue, earnedValue, _, _, isHeader, isCollapsed, hasRep, isWatched, isChild, factionID = GetFactionInfo(i)
@@ -84,37 +73,43 @@ local function ExportProfiles()
 			table.insert(HeroProfiles.factions, faction)
 		end
 	end
-	
+
 	local prof1, prof2, archaelogy, fishing, cooking = GetProfessions();
 	if (HeroProfiles.professions == nil) then
-		HeroProfiles.professions = {}
+		HeroProfiles.professions = {
+			prof1 = {
+				id = 0,
+				index = prof1,
+				name = GetProfessionInfo(prof1)
+			},
+			prof2 = {
+				id = 0,
+				index = prof2,
+				name = GetProfessionInfo(prof2)
+			},
+			cooking = {
+				id = 0,
+				index = cooking,
+				name = GetProfessionInfo(cooking)
+			},
+			fishing = {
+				id = 0,
+				index = fishing,
+				name = GetProfessionInfo(fishing)
+			}
+		}
 	end
-	if (HeroProfiles.professions.prof1 == nil) then
-		HeroProfiles.professions.prof1 = ExportProfession(prof1)
-	end
-	if (HeroProfiles.professions.prof2 == nil) then
-		HeroProfiles.professions.prof2 = ExportProfession(prof2)
-	end
-	if (HeroProfiles.professions.fishing == nil) then
-		HeroProfiles.professions.fishing = ExportProfession(fishing)
-	end
-	if (HeroProfiles.professions.cooking == nil) then
-		HeroProfiles.professions.cooking = ExportProfession(cooking)
-	end
-	
-	-- Archaelogy is a bit specific
-	HeroProfiles.professions.archaelogy = {}
-	if (archaelogy == nil) then
-		HeroProfiles.professions.archaelogy.index = nil
-		HeroProfiles.professions.archaelogy.currentLevel = nil
-		HeroProfiles.professions.archaelogy.maxLevel = nil
-	else
-		local archName, _, archCurrentLevel, archMaxLevel = GetProfessionInfo(archaelogy)
-		HeroProfiles.professions.archaelogy.index = archaelogy
-		HeroProfiles.professions.archaelogy.currentLevel = archCurrentLevel
-		HeroProfiles.professions.archaelogy.maxLevel = archMaxLevel
-	end	
-	
+
+	-- Archaelogy
+	local name, _, skillLevel, maxSkillLevel = GetProfessionInfo(archaelogy)
+	HeroProfiles.professions.archaelogy = {
+		id = 794,
+		index = archaelogy,
+		name = name,
+		skillLevel = skillLevel,
+		maxLevel = maxSkillLevel
+	}
+
 	-- Bags
 	HeroProfiles.bags = {}
 	HeroProfiles.bags.backpack = ExportBackpack()
@@ -124,11 +119,11 @@ local function ExportProfiles()
 	HeroProfiles.bags.bag4 = ExportContainer(4)
 end
 
-SlashCmdList['HERO_PROFILER'] = function()
-	ExportProfiles()
-end
+SlashCmdList_AddSlashCommand('HERO_PROFILER_SLASHCMD_EXPORT', ExportProfiles, '/hpexport')
 
-SLASH_HERO_PROFILER1 = '/heroprofiler'
+SlashCmdList_AddSlashCommand('HERO_PROFILER_SLASHCMD_CLEAR', function()
+	ClearProfiles()
+end, '/hpclear')
 
 local frame = CreateFrame("Frame");
 frame:RegisterEvent("ADDON_LOADED");
@@ -142,30 +137,30 @@ frame:Hide();
 
 frame:SetScript("OnEvent", function(self, event, ...)
 	local arg = {...}
-	
+
 	if (event == "ADDON_LOADED" and arg[1] == 'HeroProfiler') then
 		if (HeroProfiles == nil) then
 			HeroProfiles = {}
 		end
 	end
-	
+
 	if (event == "PLAYER_LOGIN") then
 		ExportProfiles()
 		RequestTimePlayed()
 	end
-	
-	if (event == "TIME_PLAYED_MSG") then 
+
+	if (event == "TIME_PLAYED_MSG") then
 		HeroProfiles.totalTime = arg[1]
 		HeroProfiles.currentLevelTime = arg[2]
 	end
-	
+
 	if (event == "PLAYER_GUILD_UPDATE") then
 		local guildName, guildRankName, guildRankIndex = GetGuildInfo("player")
 		HeroProfiles.guildName = guildName
 		HeroProfiles.guildRankName = guildRankName
 		HeroProfiles.guildRankIndex = guildRankIndex
 	end
-	
+
 	if (event == "BANKFRAME_OPENED") then
 		HeroProfiles.bags.bank1 = ExportContainer(5)
 		HeroProfiles.bags.bank2 = ExportContainer(6)
@@ -175,8 +170,8 @@ frame:SetScript("OnEvent", function(self, event, ...)
 		HeroProfiles.bags.bank6 = ExportContainer(10)
 		HeroProfiles.bags.bank7 = ExportContainer(11)
 	end
-	
-	if (event == "TRADE_SKILL_LIST_UPDATE") then	
+
+	if (event == "TRADE_SKILL_LIST_UPDATE") then
 		if (C_TradeSkillUI.IsTradeSkillReady()) then
 			levels = {}
 			local categories = { C_TradeSkillUI.GetCategories() };
@@ -190,24 +185,23 @@ frame:SetScript("OnEvent", function(self, event, ...)
 					level.currentLevel = info.skillLineCurrentLevel
 					table.insert(levels, level)
 				end
-			end			
-			
+			end
+
 			-- where to put this ?
-			local _, _, _, _, _, parentSkillLineID, parentSkillLineName =  C_TradeSkillUI.GetTradeSkillLine();
-			if (HeroProfiles.professions.prof1.name == parentSkillLineName) then
-				HeroProfiles.professions.prof1.id = parentSkillLineID
+			local _, _, _, _, _, parentID =  C_TradeSkillUI.GetTradeSkillLine();
+			if (HeroProfiles.professions.prof1.id == parentID) then
 				HeroProfiles.professions.prof1.levels = levels
-			elseif (HeroProfiles.professions.prof2.name == parentSkillLineName) then
+			elseif (HeroProfiles.professions.prof2.id == parentID) then
 				HeroProfiles.professions.prof2.id = parentSkillLineID
 				HeroProfiles.professions.prof2.levels = levels
-			elseif (HeroProfiles.professions.cooking.name == parentSkillLineName) then
+			elseif (HeroProfiles.professions.cooking.id == parentID) then
 				HeroProfiles.professions.cooking.id = parentSkillLineID
 				HeroProfiles.professions.cooking.levels = levels
-			elseif (HeroProfiles.professions.fishing.name == parentSkillLineName) then
+			elseif (HeroProfiles.professions.fishing.id == parentID) then
 				HeroProfiles.professions.fishing.id = parentSkillLineID
 				HeroProfiles.professions.fishing.levels = levels
 			end
 		end
 	end
-	
+
 end)
